5ed7a1b8a551772d66ed963c32c58b647db978f1
panyaowenhaohaoxuexi <2409854643@qq.com>
2026-06-28 15:34:15 +0800

    refresh

--- net/Network.py
+++ net/Network.py
@@ -4,7 +4,7 @@ from net.encoder.simple_encoder import SimpleSharedEncoder
 from net.fusion.text_conditioned_spatial_fusion import TGCSF as _TGCSF
 from net.decoder.simple_decoder import SimpleDecoder
 from net.intent import DualDomainTextIntentGenerator
-from net.dda import FSRC as _FSRC, FrequencySpatialResidualCoupling as _FrequencySpatialResidualCoupling
+from net.FSRC import FSRC as _FSRC, FrequencySpatialResidualCoupling as _FrequencySpatialResidualCoupling
 from net.frequency_fusion import FrequencyPyramidAdapter
 
 
